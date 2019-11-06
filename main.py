e5672552a6a9d2d1451a9ea8c2cbcfa9b22ee483
dellipi <dellipi@gmail.com>
2019-11-06 11:10:37 -0300

    Algumas alterações

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 def dec_to_bin(const):
-    aux = bin(int(const)).replace("0b", '')
+    aux = bin(int(const)).replace("0b", "")
     t = 6 - len(aux)
 
     ans = ""
     for i in range(0, t):
-        ans += '0'
+        ans += "0"
 
     ans += aux
     return ans    
 
 
 instructions = {
-    "SPC":      ["000", 'I'],
-    "TCHAN":    ["001", 'R'],
-    "ARAKETU":  ["010", 'R'],
-    "PSIRICO":  ["011", 'R'],
-    "EVA":      ["100", 'I'],
-    "ASA":      ["101", 'I'],
-    "IVETE":    ["110", 'I']
+    "SPC":      ["000", "I"],
+    "TCHAN":    ["001", "R"],
+    "ARAKETU":  ["010", "R"],
+    "PSIRICO":  ["011", "R"],
+    "EVA":      ["100", "I"],
+    "ASA":      ["101", "I"],
+    "IVETE":    ["110", "I"]
 }
 
-registers = {
+registers_info = {
     "a0": "000",
     "a1": "001",
     "a2": "010",
@@ -31,49 +31,61 @@ registers = {
     "a7": "111"
 }
 
+registers = {
+    "a0": 0,
+    "a1": 0,
+    "a2": 0,
+    "a3": 0,
+    "a4": 0,
+    "a5": 0,
+    "a6": 0,
+    "a7": 0
+}
+
 #path = input("Digite o caminho completo do arquivo: ")
 
 path = "first.xampa"
 
 f = open(path).read()
-lines = f.split('\n')
+lines = f.split("\n")
 
-program_load = [y.split(' ') for y in lines]
+program_load = [y.split(" ") for y in lines]
 program_memory = []
 
 for i in program_load:
     opcode = ""
-    if instructions[i[0]][1] == 'I':
-        opcode = instructions[i[0]][0] + registers[i[1]] + dec_to_bin(i[2])
+    if instructions[i[0]][1] == "I":
+        opcode = instructions[i[0]][0] + registers_info[i[1]] + dec_to_bin(i[2])
     else:
         if i[0] == "TCHAN":
-            opcode = instructions[i[0]][0] + registers[i[1]] + registers[i[2]]
+            opcode = instructions[i[0]][0] + registers_info[i[1]] + registers_info[i[2]]
         else:
-            opcode = instructions[i[0]][0] + registers[i[1]] + registers[i[2]] + registers[i[3]]
+            opcode = instructions[i[0]][0] + registers_info[i[1]] + registers_info[i[2]] + registers_info[i[3]]
     
     program_memory.append(opcode)
 
 program_counter = 0
 while program_counter < len(program_memory):
+    
     opcode = program_memory[program_counter]
     instruction = opcode[0:3]
 
+    # I, R ou TCHAN
+    instruction_type = ""
+
     if instruction == "000":
-
+        instruction_type = "I"
     elif instruction == "001":
-
-    elif instruction == "00":
-
-    elif instruction == "001":
-
-    elif instruction == "001":
-
-    elif instruction == "001":
-
-    elif instruction == "001":
-
-    elif instruction == "001":
-
-    elif instruction == "001":
+        instruction_type = "TCHAN"
+    elif instruction == "010":
+        instruction_type = "R"
+    elif instruction == "011":
+        instruction_type = "R"
+    elif instruction == "100":
+        instruction_type = "I"
+    elif instruction == "101":
+        instruction_type = "I"
+    elif instruction == "110":
+        instruction_type = "I"
 
     program_counter += 1
